986b5a5eba7dd1bfe79fa1d02ef7cd02b6ed4677
Jingxuan (Elaine) HOU <houjx14@mails.tsinghua.edu.cn>
2019-04-08 14:56:21 -0400

    Update state_prediction.py

--- modeling/motion_model/state_prediction.py
+++ modeling/motion_model/state_prediction.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import torch.nn as nn
+import torch
 
 from utils.data_process import *
 
@@ -17,14 +18,14 @@ class ActionSampler(nn.Module):
             nn.Linear(32,32),
             nn.ReLU(),
             nn.Linear(32,3),
-            )
+        )
 
     def sample_noise(self, actions, particles, stds, means):
         """
         Create input for action sampler network f
         Args:
-          actions: array of size (batch_size, 1, 3)
-          particles: array of size (batch_size, num_particles, 3)
+          actions: tensor of size (batch_size, 1, 3)
+          particles: tensor of size (batch_size, num_particles, 3)
 
         returns:
           sampler_input: concatenated array of size (batch_size, num_particles, 6)
@@ -32,11 +33,13 @@ class ActionSampler(nn.Module):
                          actions propagated over all particles
         """
         # Normalize actions
-        actions_input = actions / stds["actions"]
+        for i in range(3):
+            actions[:, :, i]/ = stds['a'][i]
+        actions_input = actions
         
         # Concatenate noise array to actions
         random_noise_input = torch.rand_like(actions_input)
-        sampler_input = torch.cat((actions_input, random_noise_input), axis=-1)
+        sampler_input = torch.cat((actions_input, random_noise_input), dim=-1)
         
         return sampler_input, actions_input
 
@@ -48,8 +51,8 @@ class ActionSampler(nn.Module):
         sampler_input, actions_input = self.sample_noise(actions, particles, stds, means)
 
         # Reshape concatenated array and pass to newtwork
-        sampler_input = sampler_input.view(-1, 6)
         batch_size = sampler_input.size(0)
+        sampler_input = sampler_input.view(-1, 6)
         delta_noise = self.layers(sampler_input)
         # Zero-centering of output noisy actions
         delta_noise = delta_noise - torch.mean(delta_noise)
@@ -82,21 +85,20 @@ class DynamicsModel(nn.Module):
         """
         Transform particle state to network input form
         Args:
-          particles: array of size (batch_size, num_particles, 3)
+          particles: tensor of size (batch_size, num_particles, 3)
 
         returns:
           particles_input: tensor of size (batch_size, num_particles, 4) with 
                            channels (x, y, cos(theta), sin(theta))
         """
+        for i in range(2):
+            particles[:,:,i] = (particles[:,:,i] - means["s"][i]) \
+                    / stds["s"][i]
+        norm_pos = particles[:,:,:2]
+        cos_theta = torch.cos(particles[:,:,2]).view(-1, particles.size(1), 1)
+        sin_theta = torch.sin(particles[:,:,2]).view(-1, particles.size(1), 1)
 
-        norm_pos = (particles[:,:,:2] - means["states"][:,:,:2]) \
-                    / stds["states"][:,:,:2]
-        cos_theta = torch.cos(particles[:,:,2])
-        sin_theta = torch.sin(particles[:,:,2])
-
-        particles_input = torch.cat([norm_pos, 
-                                    cos_theta,
-                                    sin_theta], axis=-1)
+        particles_input = torch.cat((norm_pos, cos_theta, sin_theta), dim=-1)
 
         return particles_input
 
@@ -110,8 +112,9 @@ class DynamicsModel(nn.Module):
           noisy_input: concatenated tensor of size (batch_size, num_particles, 8)
         """
         particles_input = self.transform_particles(particles, stds, means)
-        noisy_actions = noisy_actions / stds["actions"]
-        noisy_input = torch.cat([particles_input, noisy_actions], axis=-1)
+        for i in range(3):
+            noisy_actions[:, :, i] /= stds['a'][i]
+        noisy_input = torch.cat((particles_input, noisy_actions), dim=-1)
 
         return noisy_input
     
@@ -123,14 +126,15 @@ class DynamicsModel(nn.Module):
         
 
         # Reshape concatenated tensor and pass to network
-        noisy_input = noisy_input.view(-1, 8)
         batch_size = noisy_input.size(0)
+        noisy_input = noisy_input.view(-1, 8)
         delta_state = self.layers(noisy_input)
         # Reshape output back to original size compatiable with particles array
         delta_state = delta_state.view(batch_size, -1, 3)
         # Scaled by state step sizes Et[abs(s_t-s_{t-1})], state_step_sizes is an array of size(3,)
         # delta_state = delta_state * state_step_sizes[None, None, :]
-        delta_state = [delta_state[:, :, i:i+1] * state_step_sizes[i] for i in range(3)]
+        for i in range(3):
+            delta_state[:, :, i] *= state_step_sizes[i]
 
         moved_particles = particles + delta_state
         moved_particles[:,:,2] = wrap_angle(moved_particles[:,:,2])
